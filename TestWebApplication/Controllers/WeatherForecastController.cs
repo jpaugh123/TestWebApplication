d5ea25f0e5db431a6a52986e276d01a2d1f00e3e
Jon Paugh <secretsquirrel123@gmail.com>
2024-09-23 09:29:21 -0500

    add cold to list

--- TestWebApplication/Controllers/WeatherForecastController.cs
+++ TestWebApplication/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace TestWebApplication.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Cold", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
